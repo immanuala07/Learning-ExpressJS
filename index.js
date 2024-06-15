cd8a662b571c922848873a1d6b973229ae7a329d
Immanual <immanualatrinity@gmail.com>
2024-06-15 17:53:36 +0530

    Query parameters

--- index.js
+++ index.js
@@ -5,20 +5,38 @@ const PORT = process.env.PORT || 3500
 
 const mockUsers = [
     { id: 1, username: 'anson', displayName: 'Anson' },
-    { id: 2, username: 'aby', displayName: 'Aby' },
-    { id: 3, username: 'ken', displayName: 'Ken' },
-    { id: 4, username: 'bill', displayName: 'Bill' },
+    { id: 2, username: 'jack', displayName: 'Jack' },
+    { id: 3, username: 'adam', displayName: 'Adam' },
+    { id: 4, username: 'tina', displayName: 'Tina' },
+    { id: 5, username: 'jason', displayName: 'Jason' },
+    { id: 6, username: 'henry', displayName: 'Henry' },
+    { id: 7, username: 'marilyn', displayName: 'Marilyn' },
+    { id: 8, username: 'aby', displayName: 'Aby' },
+    { id: 9, username: 'ken', displayName: 'Ken' },
+    { id: 10, username: 'bill', displayName: 'Bill' },
 ]
 
 // GET requests
 app.get('/', (request, response) => {
     response.status(201).send({ msg: 'Hello' })
-})
+});
 
-// GET requests
+// Query Parameters in GET requests
+// link - http://localhost:3500/api/users?filter=username&value=on
 app.get('/api/users', (request, response) => {
-    response.send(mockUsers)
-})
+    console.log('request.query : ', request.query);
+    const {
+        query: { filter, value },
+    } = request
+
+    if (filter && value) {
+        return response.send(
+            mockUsers.filter((user) => user[filter].includes(value))
+        );
+    }
+
+    response.send(mockUsers);
+});
 
 // Route Parameters in GET requests
 // link - http://localhost:3500/api/users/123
@@ -38,15 +56,17 @@ app.get('/api/users/:id', (request, response) => {
     }
     console.log(findUser)
     return response.send(findUser)
-})
+});
 
 // GET requests
 app.get('/api/products', (request, response) => {
     response.send({ id: 123, name: 'Chicken', price: 12.99 })
-})
+});
 
 app.listen(PORT, () => {
     console.log(`Running on Port ${PORT}`)
-})
+});
 
-// link - http://localhost:3500/
+// http://localhost:3500/
+// http://localhost:3500/users
+// http://localhost:3500/products?key=value&key2=value2
